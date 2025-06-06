e91842ff77c43ba6d6e6d2cd5d87c13cdedbd963
SyawalN <syawalnopyandi1103@gmail.com>
2025-06-06 11:03:28 +0700

    Changed database URI format

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nluyau9.mongodb.net/maritimexpress_db?retryWrites=true&w=majority&appName=Cluster0`;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
